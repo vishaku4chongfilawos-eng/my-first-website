6dc52f5331f6439a23fc9035df40d42112f74b70
vishaku4chongfilawos-eng <vishaku4chongfilawos@gmail.com>
2026-03-11 14:46:58 +0100

    update website

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function greetuser() {
-    // Get value from input box 
-    const username = document.getElementById("username").value;
-    // show message in paragragh
-    console.log(username);
-    document.getElementById("message").innerText =" " + username + "! My name is Victor Ishaku, I'm learning Web Development";
-}
-
-function myDetails(){
-    const msg2 = document.getElementById("message2");
-    msg2.innerText="I love Football and I am an arsenal fan and I know arsenal is winning the league this season 2025/2026";
-    msg2.style.color = "green";
-    msg2.style.fontSize = "18px";
-}
-function showMessage3() {
-    const msg3 = document.getElementById("message3");
-    msg3.innerText="This is my small background story; My name is Victor Ishaku, a dedicated Computer Science student and aspiring Web Developer";
-    msg3.style.color = "darkblue";
-    msg3.style.fontSize = "18px";
-    msg3.style.marginTop = "10px";
-}
+  // Display the greeting message    messageElement.textContent = `${greeting}! Welcome to my portfolio website.`;
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 darkModeToggle.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
@@ -29,3 +9,22 @@ darkModeToggle.addEventListener("click", function() {
         darkModeToggle.textContent = "Dark Mode";
     }
 });
+const text = ["Computer Scientist", "Web Developer", "front-end learner"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+(function type() {
+    if(count === text.length) {
+        count = 0;
+    }
+    currentText = text[count];
+    letter = currentText.slice(0, index);
+    document.getElementById("typing").textContent = letter;
+    index++;
+    if(letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 100);
+})();
